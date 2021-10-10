1025213cdaedb5f013b6c7a1fd283fe54499265a
Lucas Unnerfelt <llasarus@gmail.com>
2021-10-10 16:05:28 +0200

    Made precedence.c easier to read.

--- src/precedence.c
+++ src/precedence.c
@@ -1,61 +1,27 @@
 #include "precedence.h"
 
 // Taken from https://en.wikipedia.org/wiki/Operators_in_C_and_C%2B%2B
+static const int precedence_table[T_COUNT] = {
+	[T_INC] = 18, [T_DEC] = 18, [T_LPAR] = 18, [T_LBRACK] = 18, [T_ARROW] = 18, [T_DOT] = 18,
+	[T_STAR] = 16, [T_DIV] = 16, [T_MOD] = 16,
+	[T_ADD] = 15, [T_SUB] = 15,
+	[T_LSHIFT] = 14, [T_RSHIFT] = 14,
+	[T_LEQ] = 13, [T_L] = 13, [T_G] = 13, [T_GEQ] = 13,
+	[T_EQ] = 12, [T_NEQ] = 12,
+	[T_AMP] = 11,
+	[T_XOR] = 10,
+	[T_BOR] = 9,
+	[T_AND] = 8,
+	[T_OR] = 7,
+	[T_A] = 6, [T_ADDA] = 6, [T_SUBA] = 6, [T_MULA] = 6,
+	[T_DIVA] = 6, [T_MODA] = 6, [T_LSHIFTA] = 6, [T_RSHIFTA] = 6,
+	[T_BORA] = 6, [T_XORA] = 6, [T_BANDA] = 6, [T_QUEST] = 6,
+	[T_COMMA] = 5,
+};
 
 int precedence_get(enum ttype token_type, int loop) {
-	switch (token_type) {
-	case T_INC:
-	case T_DEC:
-	case T_LPAR:
-	case T_LBRACK:
-	case T_ARROW:
-	case T_DOT:
-		return 18; // It is really a postfix.
-	case T_STAR:
-	case T_DIV:
-	case T_MOD:
-		return 16;
-	case T_ADD:
-	case T_SUB:
-		return 15;
-	case T_LSHIFT:
-	case T_RSHIFT:
-		return 14;
-	case T_LEQ:
-	case T_L:
-	case T_G:
-	case T_GEQ:
-		return 13;
-	case T_EQ:
-	case T_NEQ:
-		return 12;
-	case T_AMP:
-		return 11;
-	case T_XOR:
-		return 10;
-	case T_BOR:
-		return 9;
-	case T_AND:
-		return 8;
-	case T_OR:
-		return 7;
-	case T_A:
-	case T_ADDA:
-	case T_SUBA:
-	case T_MULA:
-	case T_DIVA:
-	case T_MODA:
-	case T_LSHIFTA:
-	case T_RSHIFTA:
-	case T_BORA:
-	case T_XORA:
-	case T_BANDA:
-	case T_QUEST:
-		return 6 - (loop ? 0 : 1);
-	case T_COMMA:
+	int prec = precedence_table[token_type];
+	if (!loop && prec == 6)
 		return 5;
-	default:
-		return -1;
-	}
-	return 0;
+	return prec;
 }
