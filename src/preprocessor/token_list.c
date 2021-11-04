4ccc122facfd6077bc9a684c771f401484b5fd8d
Lucas Unnerfelt <llasarus@gmail.com>
2021-11-05 00:03:02 +0100

    Remove unused functions from token_list.c

--- src/preprocessor/token_list.c
+++ src/preprocessor/token_list.c
@@ -10,20 +10,8 @@ void token_list_add(struct token_list *list, struct token t) {
 	ADD_ELEMENT(list->size, list->cap, list->list) = t;
 }
 
-struct token *token_list_top(struct token_list *list) {
-	return &list->list[list->size - 1];
-}
-
 void token_list_pop(struct token_list *list) {
 	list->size--;
-}
-
-void token_list_push_front(struct token_list *list, struct token t) {
-	token_list_add(list, t);
-	for (int i = list->size - 1; i >= 1; i--) {
-		list->list[i] = list->list[i - 1];
-	}
-	list->list[0] = t;
 }
 
 int token_list_index_of(struct token_list *list, struct token t) {
--- src/preprocessor/token_list.h
+++ src/preprocessor/token_list.h
@@ -10,9 +10,7 @@ struct token_list {
 
 void token_list_free(struct token_list *list);
 void token_list_add(struct token_list *list, struct token t);
-struct token *token_list_top(struct token_list *list);
 void token_list_pop(struct token_list *list);
-void token_list_push_front(struct token_list *list, struct token t);
 int token_list_index_of(struct token_list *list, struct token t);
 
 #endif
